00c0a3670a8ba10da9b303362615b72c4dfa03e7
Costin-Emanuel VASILE <costin.vasile1003@upb.ro>
2025-10-14 18:25:11 +0300

    Removed the RTL files from files.f

--- run/files.f
+++ run/files.f
@@ -1,9 +1,3 @@
-# Auto-added generated IP VHDL (do not edit by hand)
-../src/rtl/axi_gpio_v2_0_vh_rfs.vhd
-../src/rtl/axi_lite_ipif_v3_0_vh_rfs.vhd
-../src/rtl/interrupt_control_v3_1_vh_rfs.vhd
-../src/rtl/lib_cdc_v1_0_rfs.vhd
-
 # Testbench files (compile interface first, then package, then top)
 ../src/testbench/if/my_if.sv
 ../src/testbench/uvm_pkg/uvm_tb_pkg.sv
