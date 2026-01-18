d0dfded228137bf4f3671e1aa88c6a9f597a4ffa
Abomosa ABDELRAHMAN <aabdelrahman@station05.fsc00.arh.pub.ro>
2026-01-18 21:44:18 +0200

    codex

---
+++ run/sim/xsim.dir/work.top/xsim_script.tcl
@@ -0,0 +1,1 @@
+xsim {work.top} -testplusarg +UVM_TESTNAME=base_test -testplusarg +UVM_VERBOSITY=UVM_HIGH -wdb {xsim.wdb} -autoloadwcfg -tclbatch {/ghome/aabdelrahman/Abdo/dydy/run/sim/run_batch.tcl}
